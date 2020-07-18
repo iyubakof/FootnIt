ba8930cf1ce4636af6829d8a744be30babe2cc85
Yvonne Ubak <iyvonneuo@gmail.com>
2020-07-18 12:03:34 -0400

    Change title

--- src/constants/Navigation.js
+++ src/constants/Navigation.js
@@ -8,7 +8,7 @@ export default class Navigation extends Component {
             <>
                 <ul className="navbar">
                     <li className="nav-item"><Link to='/recipes' className='link'>Recipes</Link></li>
-                    <li className="nav-brand"><Link to='/' className='link'>Grandma's Book</Link></li>
+                    <li className="nav-brand"><Link to='/' className='link'>Foot N' It</Link></li>
                     <li className="nav-item"><Link to='/cuisines' className='link'>Cuisines</Link></li>
                 </ul>
             </>
