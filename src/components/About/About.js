8754704195394b31747f2ec14122d6ff7f218682
Yvonne Ubak <iyvonneuo@gmail.com>
2020-07-24 20:11:46 -0400

    changed api and updated About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './About.scss';
 
 export class About extends Component {
     render() {
         return (
-            <div>
-                <h1>About...</h1>
-            </div>
+            <>
+                <div className="intro-wrapper">
+                    <h1>Recipe Finder</h1>
+                    <div className="subtext">
+                        <h5>Need help cooking tonight?</h5>
+                        <p>Pick from the random recipes below or search by cuisine</p>
+                    </div>
+                </div>
+            </>
         )
     }
 }
